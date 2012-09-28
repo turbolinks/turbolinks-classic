53312e59274fe58645a6ada7c97fa12014812b08
David Estes <destes@redwindsw.com>
2012-09-28 09:05:40 -0400

    added server side overrides for redirect to related bugs

--- lib/turbolinks.rb
+++ lib/turbolinks.rb
@@ -1,4 +1,12 @@
+
+require 'turbolinks/redirect'
 module Turbolinks
   class Engine < ::Rails::Engine
+    initializer :turbolinks_headers do |config|
+      ActionController::Base.class_eval do
+        include Turbolinks::PushStateFilter
+        after_filter :set_push_state_location
+      end
+    end
   end
 end
