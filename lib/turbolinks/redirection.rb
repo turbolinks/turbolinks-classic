04f9a5cf36159ccbd257afe55713cb15099fc179
Thibaut <thibaut@me.com>
2015-02-26 18:14:52 -0500

    Make redirect_to perform a turbolinks visit on xhr+non-get requests

--- lib/turbolinks/redirection.rb
+++ lib/turbolinks/redirection.rb
@@ -3,10 +3,23 @@ module Turbolinks
   # will respond with a JavaScript call to Turbolinks.visit(url).
   module Redirection
     extend ActiveSupport::Concern
-    
+
+    def redirect_to(url = {}, response_status = {})
+      super(url, response_status)
+
+      if request.xhr? && !request.get?
+        perform_turbolinks_visit
+      end
+    end
+
     def redirect_via_turbolinks_to(url = {}, response_status = {})
       redirect_to(url, response_status)
+      perform_turbolinks_visit
+    end
 
+    private
+
+    def perform_turbolinks_visit
       self.status           = 200
       self.response_body    = "Turbolinks.visit('#{location}');"
       response.content_type = Mime::JS
--- test/turbolinks/redirection_test.rb
+++ test/turbolinks/redirection_test.rb
@@ -12,10 +12,42 @@ class RedirectController < ActionController::Base
   def redirect_via_turbolinks_to_path_and_custom_status
     redirect_via_turbolinks_to '/path', status: 303
   end
+
+  def simple_redirect
+    redirect_to action: 'action'
+  end
 end
 
 class RedirectionTest < ActionController::TestCase
   tests RedirectController
+
+  def test_redirect_to_via_xhr_and_post_redirects_via_turbolinks
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    post :simple_redirect
+    assert_turbolinks_visit 'http://test.host/redirect/action'
+  end
+
+  def test_redirect_to_via_xhr_and_patch_redirects_via_turbolinks
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    patch :simple_redirect
+    assert_turbolinks_visit 'http://test.host/redirect/action'
+  end
+
+  def test_redirect_to_via_xhr_and_get_does_normal_redirect
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    get :simple_redirect
+    assert_redirected_to 'http://test.host/redirect/action'
+  end
+
+  def test_redirect_to_via_post_and_not_xhr_does_normal_redirect
+    post :simple_redirect
+    assert_redirected_to 'http://test.host/redirect/action'
+  end
+
+  def test_redirect_to_via_patch_and_not_xhr_does_normal_redirect
+    patch :simple_redirect
+    assert_redirected_to 'http://test.host/redirect/action'
+  end
 
   def test_redirect_via_turbolinks_to_url_string
     get :redirect_via_turbolinks_to_url_string
