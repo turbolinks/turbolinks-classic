200c43c4f431efc2bb8a8d48c1afc2ab5e034750
Thibaut <thibaut@me.com>
2015-02-26 19:40:51 -0500

    redirect_via_turbolinks_to -> redirect_to turbolinks: true

--- lib/turbolinks/redirection.rb
+++ lib/turbolinks/redirection.rb
@@ -5,27 +5,15 @@ module Turbolinks
     extend ActiveSupport::Concern
 
     def redirect_to(url = {}, response_status = {})
-      change = response_status.delete(:change)
+      options = response_status.extract!(:change, :turbolinks)
       super(url, response_status)
 
-      if request.xhr? && !request.get?
-        perform_turbolinks_visit(change)
+      if options[:turbolinks] || (request.xhr? && !request.get?)
+        change = ", { change: ['#{Array(options[:change]).join("', '")}'] }" if options[:change]
+        self.status           = 200
+        self.response_body    = "Turbolinks.visit('#{location}'#{change});"
+        response.content_type = Mime::JS
       end
-    end
-
-    def redirect_via_turbolinks_to(url = {}, response_status = {})
-      change = response_status.delete(:change)
-      redirect_to(url, response_status)
-      perform_turbolinks_visit(change)
-    end
-
-    private
-
-    def perform_turbolinks_visit(change = nil)
-      change = ", { change: ['#{Array(change).join("', '")}'] }" if change
-      self.status           = 200
-      self.response_body    = "Turbolinks.visit('#{location}'#{change});"
-      response.content_type = Mime::JS
     end
   end
 end
--- test/turbolinks/redirection_test.rb
+++ test/turbolinks/redirection_test.rb
@@ -1,116 +1,115 @@
 require_relative 'test_helper'
 
 class RedirectController < ActionController::Base
-  def redirect_via_turbolinks_to_url_string
-    redirect_via_turbolinks_to 'http://example.com'
+  def redirect_to_url_string
+    redirect_to 'http://example.com'
   end
 
-  def redirect_via_turbolinks_to_url_hash
-    redirect_via_turbolinks_to action: 'action'
+  def redirect_to_url_string_with_turbolinks
+    redirect_to 'http://example.com', turbolinks: true
   end
 
-  def redirect_via_turbolinks_to_path_and_custom_status
-    redirect_via_turbolinks_to '/path', status: 303
-  end
-
-  def redirect_via_turbolinks_to_path_with_single_change_option
-    redirect_via_turbolinks_to '/path', change: 'foo'
-  end
-
-  def redirect_via_turbolinks_to_path_with_multiple_change_option
-    redirect_via_turbolinks_to '/path', change: ['foo', :bar]
-  end
-
-  def simple_redirect
+  def redirect_to_url_hash
     redirect_to action: 'action'
   end
 
-  def simple_redirect_with_single_change_option
-    redirect_to({action: 'action'}, change: 'foo')
+  def redirect_to_url_hash_with_turbolinks
+    redirect_to({action: 'action'}, turbolinks: true)
   end
 
-  def simple_redirect_with_multiple_change_option
-    redirect_to({action: 'action'}, change: ['foo', :bar])
+  def redirect_to_path_and_custom_status
+    redirect_to '/path', status: 303
+  end
+
+  def redirect_to_path_with_single_change_option
+    redirect_to '/path', change: 'foo'
+  end
+
+  def redirect_to_path_with_multiple_change_option
+    redirect_to '/path', change: ['foo', :bar]
+  end
+
+  def redirect_to_path_with_change_option_and_custom_status
+    redirect_to '/path', change: ['foo', :bar], status: 303
   end
 end
 
 class RedirectionTest < ActionController::TestCase
   tests RedirectController
 
-  def test_redirect_to_via_xhr_and_post_redirects_via_turbolinks
-    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
-    post :simple_redirect
+  def test_redirect_to_url_string_with_turbolinks
+    get :redirect_to_url_string_with_turbolinks
+    assert_turbolinks_visit 'http://example.com'
+  end
+
+  def test_redirect_to_url_hash_with_turbolinks
+    get :redirect_to_url_hash_with_turbolinks
     assert_turbolinks_visit 'http://test.host/redirect/action'
   end
 
-  def test_redirect_to_via_xhr_and_patch_redirects_via_turbolinks
+  def test_redirect_to_url_string_via_xhr_and_post_redirects_via_turbolinks
     @request.headers['X-Requested-With'] = 'XMLHttpRequest'
-    patch :simple_redirect
+    post :redirect_to_url_string
+    assert_turbolinks_visit 'http://example.com'
+  end
+
+  def test_redirect_to_url_hash_via_xhr_and_patch_redirects_via_turbolinks
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    patch :redirect_to_url_hash
     assert_turbolinks_visit 'http://test.host/redirect/action'
+  end
+
+  def test_redirect_to_path_and_custom_status_via_xhr_and_delete_redirects_via_turbolinks
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    delete :redirect_to_path_and_custom_status
+    assert_turbolinks_visit 'http://test.host/path'
   end
 
   def test_redirect_to_via_xhr_and_get_does_normal_redirect
     @request.headers['X-Requested-With'] = 'XMLHttpRequest'
-    get :simple_redirect
-    assert_redirected_to 'http://test.host/redirect/action'
+    get :redirect_to_path_and_custom_status
+    assert_response 303
+    assert_redirected_to 'http://test.host/path'
   end
 
   def test_redirect_to_via_post_and_not_xhr_does_normal_redirect
-    post :simple_redirect
+    post :redirect_to_url_hash
     assert_redirected_to 'http://test.host/redirect/action'
   end
 
   def test_redirect_to_via_patch_and_not_xhr_does_normal_redirect
-    patch :simple_redirect
-    assert_redirected_to 'http://test.host/redirect/action'
+    patch :redirect_to_url_string
+    assert_redirected_to 'http://example.com'
   end
 
   def test_redirect_to_via_xhr_and_post_with_single_change_option
     @request.headers['X-Requested-With'] = 'XMLHttpRequest'
-    post :simple_redirect_with_single_change_option
-    assert_turbolinks_visit 'http://test.host/redirect/action', "{ change: ['foo'] }"
+    post :redirect_to_path_with_single_change_option
+    assert_turbolinks_visit 'http://test.host/path', "{ change: ['foo'] }"
   end
 
   def test_redirect_to_via_xhr_and_post_with_multiple_change_option
     @request.headers['X-Requested-With'] = 'XMLHttpRequest'
-    post :simple_redirect_with_multiple_change_option
-    assert_turbolinks_visit 'http://test.host/redirect/action', "{ change: ['foo', 'bar'] }"
-  end
-
-  def test_redirect_to_via_post_and_not_xhr_with_single_change_option
-    post :simple_redirect_with_single_change_option
-    assert_redirected_to 'http://test.host/redirect/action'
-  end
-
-  def test_redirect_to_via_xhr_and_get_with_multiple_change_option
-    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
-    get :simple_redirect_with_multiple_change_option
-    assert_redirected_to 'http://test.host/redirect/action'
-  end
-
-  def test_redirect_via_turbolinks_to_url_string
-    get :redirect_via_turbolinks_to_url_string
-    assert_turbolinks_visit 'http://example.com'
-  end
-
-  def test_redirect_via_turbolinks_to_url_hash
-    get :redirect_via_turbolinks_to_url_hash
-    assert_turbolinks_visit 'http://test.host/redirect/action'
-  end
-
-  def test_redirect_via_turbolinks_to_path_and_custom_status
-    get :redirect_via_turbolinks_to_path_and_custom_status
-    assert_turbolinks_visit 'http://test.host/path'
-  end
-
-  def test_redirect_via_turbolinks_to_path_with_single_change_option
-    get :redirect_via_turbolinks_to_path_with_single_change_option
-    assert_turbolinks_visit 'http://test.host/path', "{ change: ['foo'] }"
-  end
-
-  def test_redirect_via_turbolinks_to_path_with_multiple_change_option
-    get :redirect_via_turbolinks_to_path_with_multiple_change_option
+    post :redirect_to_path_with_multiple_change_option
     assert_turbolinks_visit 'http://test.host/path', "{ change: ['foo', 'bar'] }"
+  end
+
+  def test_redirect_to_via_xhr_and_post_with_change_option_and_custom_status
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    post :redirect_to_path_with_change_option_and_custom_status
+    assert_turbolinks_visit 'http://test.host/path', "{ change: ['foo', 'bar'] }"
+  end
+
+  def test_redirect_to_via_xhr_and_get_with_change_option
+    @request.headers['X-Requested-With'] = 'XMLHttpRequest'
+    get :redirect_to_path_with_multiple_change_option
+    assert_redirected_to 'http://test.host/path'
+  end
+
+  def test_redirect_to_via_post_and_not_xhr_with_change_option_and_custom_status
+    post :redirect_to_path_with_change_option_and_custom_status
+    assert_response 303
+    assert_redirected_to 'http://test.host/path'
   end
 
   private
