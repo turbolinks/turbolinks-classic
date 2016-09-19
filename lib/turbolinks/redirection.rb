471e0b8cde1d2a7265acb8f390e493631a33dec9
Adam Becker <ad@mbecker.cc>
2016-09-19 15:28:41 -0700

    fix rails 5 deprecations

--- lib/turbolinks/redirection.rb
+++ lib/turbolinks/redirection.rb
@@ -3,13 +3,13 @@ module Turbolinks
   # will respond with a JavaScript call to Turbolinks.visit(url).
   module Redirection
     extend ActiveSupport::Concern
-    
+
     def redirect_via_turbolinks_to(url = {}, response_status = {})
       redirect_to(url, response_status)
 
       self.status           = 200
       self.response_body    = "Turbolinks.visit('#{location}');"
-      response.content_type = Mime::JS
+      response.content_type = Mime[:js]
     end
   end
 end
